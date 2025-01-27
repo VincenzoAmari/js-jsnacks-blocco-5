85347d826a5db04a70b04c729a16c2bcbc6226a1
VincenzoAmari <vincenzo.am99@gmail.com>
2025-01-27 16:38:04 +0100

    snack 1

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,11 @@
-const names = ['Edoardo', 'Simone', 'Francesco'];
+const names = ["Edoardo", "Simone", "Francesco"];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+names.forEach((element) => console.log(element));
+
+// versione estesa
+// names.forEach(function (element) {
+//   console.log(element);
+// });
